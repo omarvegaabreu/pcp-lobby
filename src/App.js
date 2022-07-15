3e38ae2f9a7e0ef1e23cfbdcd91df39c533b12c7
omarvegaabreu <omarvega@hotmail.com>
2022-07-14 21:48:14 -0400

    added mock request

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      patients: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((patientApi) =>
+        this.setState(
+          () => {
+            return { patients: patientApi };
+          },
+          () => console.log(this.state.patients)
+        )
+      );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.patients.map((patient) => {
+          return <h1 key={patient.id}>{patient.name}</h1>;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
