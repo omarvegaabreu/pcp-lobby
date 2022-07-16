6a7dd2af413ccbf32c42d9376345f91d2c21b2e1
omarvegaabreu <omarvega@hotmail.com>
2022-07-15 22:17:07 -0400

    added mock api call

--- src/App.js
+++ src/App.js
@@ -4,31 +4,54 @@ import "./App.css";
 
 class App extends React.Component {
   constructor() {
+    console.log("constructor");
     super();
 
     this.state = {
       patients: [],
+      searchField: [],
     };
   }
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((patientApi) =>
-        this.setState(
-          () => {
-            return { patients: patientApi };
-          },
-          () => console.log(this.state.patients)
-        )
+      .then((patientsData) =>
+        this.setState(() => {
+          return { patients: patientsData };
+        })
       );
+    console.log("componentDidMount");
   }
 
+  onInputChange = (event) => {
+    const searchString = event.target.value.toLowerCase();
+
+    this.setState(() => {
+      return { searchField: searchString };
+    });
+  };
+
   render() {
+    const { patients, searchField } = this.state;
+    const { onInputChange } = this;
+
+    const filteredPatient = patients.filter((patient) => {
+      return patient.name.toLowerCase().includes(searchField);
+    });
+
+    console.log(filteredPatient);
+    console.log("render");
     return (
       <div className="App">
-        {this.state.patients.map((patient) => {
-          return <h1 key={patient.id}>{patient.name}</h1>;
+        <input
+          type="text"
+          className="patients"
+          placeholder="Search patients"
+          onChange={onInputChange}
+        />
+        {filteredPatient.map((search) => {
+          return <h1 key={search.id}>{search.name}</h1>;
         })}
       </div>
     );
